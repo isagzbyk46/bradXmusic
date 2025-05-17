df54139e2f967428aec4216cdb19972aab1a0128
idk20256 <emailmati98@gmail.com>
2025-05-17 22:58:26 +0700

    _

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/idk20256/xx",
+    "https://github.com/idk20256/bradXmusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
